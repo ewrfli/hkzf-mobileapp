3e0c6568d113a5aadd8de53a0cfe8abdde4d5a2e
ewrf3 <q304062@163.com>
2021-08-09 01:09:14 +0800

    nav commit

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
-import { Carousel } from 'antd-mobile';
+import { Carousel, Flex } from 'antd-mobile';
 
 import axios from 'axios'
+import Nav1 from '../../assets/images/nav-1.png'
+import Nav2 from '../../assets/images/nav-2.png'
+import Nav3 from '../../assets/images/nav-3.png'
+import Nav4 from '../../assets/images/nav-4.png'
+// 导入样式文件
+import './index.css'
+
 export default class Index extends React.Component {
 
   state = {
@@ -12,7 +19,7 @@ export default class Index extends React.Component {
 
   //获取轮播图数据的方法
   async getSwipers () {
-    const res = await axios.get('http://localhost:8080/home/swiper')
+    const res = await axios.get('http://101.132.119.44:9090/home/swiper')
     // console.log(res)
     this.setState(() => {
       return {
@@ -33,7 +40,7 @@ export default class Index extends React.Component {
     return this.state.swipers.map(item => (
       <a
         key={item.id}
-        href="http://itcast.cn"
+        href="http://101.132.119.44:9090"
         style={{
           display: 'inline-block',
           width: '100%',
@@ -41,7 +48,7 @@ export default class Index extends React.Component {
         }}
       >
         <img
-          src={`http://localhost:8080${item.imgSrc}`}
+          src={`http://101.132.119.44:9090${item.imgSrc}`}
           alt=""
           style={{ width: '100%', verticalAlign: 'top' }}
         />
@@ -51,6 +58,8 @@ export default class Index extends React.Component {
   render () {
     return (
       <div className='index'>
+
+        {/* 轮播图 */}
         <Carousel
           autoplay={true}
           infinite
@@ -58,6 +67,28 @@ export default class Index extends React.Component {
         >
           {this.renderSwipers()}
         </Carousel>
+
+        {/* 导航菜单 */}
+        <Flex className="nav">
+          <Flex.Item>
+            <img src={Nav1} alt="" />
+            <h2>整租</h2>
+          </Flex.Item>
+          <Flex.Item>
+            <img src={Nav2} alt="" />
+            <h2>合租</h2>
+          </Flex.Item>
+          <Flex.Item>
+            <img src={Nav3} alt="" />
+            <h2>地图找房</h2>
+          </Flex.Item>
+          <Flex.Item>
+            <img src={Nav4} alt="" />
+            <h2>去出租</h2>
+          </Flex.Item>
+
+        </Flex>
+
       </div>
     );
   }
